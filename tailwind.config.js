dbbf171f6d15325b598acba2b95f1b4777f476f7
mmaleszewski <mmaleszewski@trans.eu>
2025-03-01 08:49:46 +0100

    feat: add Tailwind CSS configuration with custom color palette

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "sky-blue": "#8ecae6",
+        "blue-green": "#219ebc",
+        "prussian-blue": "#023047",
+        "selective-yellow": "#ffb703",
+        "ut-orange": "#fb8500",
+      },
+    },
+  },
+  // ...rest of your config
+};
