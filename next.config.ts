c011a7db24f932cf30ba4a098f5dcaad2e36407a
mmaleszewski <mmaleszewski@trans.eu>
2025-03-02 18:57:57 +0100

    feat: add environment variable for socket server URL in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_SOCKET_URL: 'http://localhost:3001', // Update with your actual socket server URL
+  },
 };
 
 export default nextConfig;
