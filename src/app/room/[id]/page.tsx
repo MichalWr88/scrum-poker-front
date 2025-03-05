343f7fd5b972e5109ed6fc3c5085a72d5dc9cb3f
mmaleszewski <mmaleszewski@trans.eu>
2025-03-05 13:15:34 +0100

    feat: update RoomPage to use params for dynamic room ID display

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,15 +1,15 @@
 import { ScrumPokerRoom } from "@/components/scrum-poker-room";
+import { PageProps } from "../../../../.next/types/app/page";
 
-const RoomPage = async ({ params }: { params: { id: string } }) => {
-  const { id } = await params;
+export default async function RoomPage(props: PageProps) {
+  const bb = await props.params;
+
   return (
-    <div className=" h-screen flex-col overflow-auto p-3">
+    <div className="h-screen flex-col overflow-auto p-3">
       <h1 className="text-2xl font-bold text-blue-900">
-        Planning Poker: Room {id}
+        Planning Poker: Room {bb.id}
       </h1>
       <ScrumPokerRoom />
     </div>
   );
-};
-
-export default RoomPage;
+}
