42a46bbe5364fa54697f6a824095f4d0f2a53c6f
mmaleszewski <maleszewski.geo@gmail.com>
2025-03-09 08:04:35 +0100

    feat: add StatisticsWrapper component for displaying voting statistics

---
+++ src/components/room/statistics/statistics-wrapper.tsx
@@ -0,0 +1,43 @@
+import { Vote } from "@/services/socket/socket-service";
+import React from "react";
+
+type Props = {
+  participants: Vote[];
+};
+
+const StatisticsWrapper = ({ participants }: Props) => {
+  return (
+    <div className="bg-white rounded-lg shadow p-6">
+      <h2 className="text-xl font-semibold text-blue-900 mb-4">Statistics</h2>
+      <div className="flex flex-col gap-4">
+        <div>
+          <p className="text-sm text-sky-600">Average</p>
+          <p className="font-medium text-blue-900">
+            {participants
+              .filter((p) => !!p.value)
+              .reduce(
+                (sum, p) =>
+                  sum + ((p.value && parseFloat(p.value.toString())) || 0),
+                0
+              ) / participants.length}
+          </p>
+        </div>
+        <div>
+          <p className="text-sm text-sky-600">Median</p>
+          <p className="font-medium text-blue-900">
+            {
+              participants
+                .filter((p) => !!p.value)
+                .map((p) =>
+                  p.value !== null ? parseFloat(p.value.toString()) : 0
+                )
+                .sort((a, b) => a - b)[Math.floor(participants.length / 2)]
+            }
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StatisticsWrapper;
