11095f8cc1b5b03c337238ec9955d2d323b66647
mmaleszewski <maleszewski.geo@gmail.com>
2025-03-09 08:05:06 +0100

    feat: update RoomPage to include RoomWrapper, ActionButtons, and VotingArea components

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,15 +1,24 @@
-import { ScrumPokerRoom } from "@/components/scrum-poker-room";
 import { PageProps } from "../../../../.next/types/app/page";
+import JiraTask from "@/components/jira/jira-task";
+import RoomWrapper from "@/components/room/room-wrapper";
+import ActionButtons from "@/components/room/action-buttons";
+import VotingArea from "@/components/room/voting-area";
 
 export default async function RoomPage(props: PageProps) {
-  const bb = await props.params;
+  const params = await props.params;
 
   return (
     <div className="h-screen flex-col overflow-auto p-3">
       <h1 className="text-2xl font-bold text-blue-900">
-        Planning Poker: Room {bb.id}
+        Planning Poker: Room {params.id}
       </h1>
-      <ScrumPokerRoom />
+      <RoomWrapper
+        actionComponent={<ActionButtons />}
+        votingComponent={<VotingArea />}
+      >
+        <JiraTask />
+      </RoomWrapper>
+
     </div>
   );
 }
