fa473a42a71061f169365ebc0484e6fcc900e5f3
mmaleszewski <maleszewski.geo@gmail.com>
2025-03-10 08:48:32 +0100

    feat: conditionally render statistics for participants in StatisticsWrapper component

--- src/components/room/statistics/statistics-wrapper.tsx
+++ src/components/room/statistics/statistics-wrapper.tsx
@@ -9,33 +9,35 @@ const StatisticsWrapper = ({ participants }: Props) => {
   return (
     <div className="bg-white rounded-lg shadow p-6">
       <h2 className="text-xl font-semibold text-blue-900 mb-4">Statistics</h2>
-      <div className="flex flex-col gap-4">
-        <div>
-          <p className="text-sm text-sky-600">Average</p>
-          <p className="font-medium text-blue-900">
-            {participants
-              .filter((p) => !!p.value)
-              .reduce(
-                (sum, p) =>
-                  sum + ((p.value && parseFloat(p.value.toString())) || 0),
-                0
-              ) / participants.length}
-          </p>
-        </div>
-        <div>
-          <p className="text-sm text-sky-600">Median</p>
-          <p className="font-medium text-blue-900">
-            {
-              participants
+      {participants.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <div>
+            <p className="text-sm text-sky-600">Average</p>
+            <p className="font-medium text-blue-900">
+              {participants
                 .filter((p) => !!p.value)
-                .map((p) =>
-                  p.value !== null ? parseFloat(p.value.toString()) : 0
-                )
-                .sort((a, b) => a - b)[Math.floor(participants.length / 2)]
-            }
-          </p>
+                .reduce(
+                  (sum, p) =>
+                    sum + ((p.value && parseFloat(p.value.toString())) || 0),
+                  0
+                ) / participants.length}
+            </p>
+          </div>
+          <div>
+            <p className="text-sm text-sky-600">Median</p>
+            <p className="font-medium text-blue-900">
+              {
+                participants
+                  .filter((p) => !!p.value)
+                  .map((p) =>
+                    p.value !== null ? parseFloat(p.value.toString()) : 0
+                  )
+                  .sort((a, b) => a - b)[Math.floor(participants.length / 2)]
+              }
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
