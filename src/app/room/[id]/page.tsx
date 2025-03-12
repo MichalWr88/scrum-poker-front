b4b09b29822be1bd1f8a884e06e04844770d1d6c
mmaleszewski <maleszewski.geo@gmail.com>
2025-03-12 09:19:55 +0100

    feat: simplify RoomPage component by removing unused props and header

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,17 +1,11 @@
-import { PageProps } from "../../../../.next/types/app/page";
 import JiraTask from "@/components/jira/jira-task";
 import RoomWrapper from "@/components/room/room-wrapper";
 import ActionButtons from "@/components/room/action-buttons";
 import VotingArea from "@/components/room/voting-area";
 
-export default async function RoomPage(props: PageProps) {
-  const params = await props.params;
-
+export default async function RoomPage() {
   return (
     <div className="h-screen flex-col overflow-auto p-3">
-      <h1 className="text-2xl font-bold text-blue-900">
-        Planning Poker: Room {params.id}
-      </h1>
       <RoomWrapper
         actionComponent={<ActionButtons />}
         votingComponent={<VotingArea />}
