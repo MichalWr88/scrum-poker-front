4e9d1a23f483680b2294290621440e0369370215
mmaleszewski <maleszewski.geo@gmail.com>
2025-03-12 09:09:04 +0100

    feat: update default socket URL to localhost:3002

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_SOCKET_URL:
-      process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001", // Update with your actual socket server URL
+      process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3002", // Update with your actual socket server URL
   },
 };
 
