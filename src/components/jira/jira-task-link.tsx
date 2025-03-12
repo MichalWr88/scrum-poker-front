8899c9eac772c73feef3e4fb714c3f20e475f6de
mmaleszewski <maleszewski.geo@gmail.com>
2025-03-12 09:16:21 +0100

    feat: enhance JiraTaskLink component with hover effect

--- src/components/jira/jira-task-link.tsx
+++ src/components/jira/jira-task-link.tsx
@@ -11,11 +11,8 @@ const JiraTaskLink = ({ jiraKey }: Props) => {
       href={`https://jira.trans.eu/browse/${jiraKey}`}
       target="_blank"
       rel="noopener noreferrer"
-      className="ml-2 w-6 h-6"
+      className="ml-2 w-6 h-6 hover:scale-125 transition-all transform"
       title="Link to Jira"
-      style={{ transition: "transform 0.2s" }}
-      onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.1)")}
-      onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
     >
       <JiraIcon />
     </a>
