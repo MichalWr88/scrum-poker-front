1ef21a34c5f98d4c60e87d7b6e5cb1980c8aedf8
mmaleszewski <maleszewski.geo@gmail.com>
2025-03-12 10:32:22 +0100

    feat: clean up RoomWrapper component by removing unnecessary comments

--- src/components/room/room-wrapper.tsx
+++ src/components/room/room-wrapper.tsx
@@ -9,7 +9,6 @@ const RoomWrapper = ({
 }) => {
   return (
     <div className="flex h-full gap-4">
-      {/* Left side (70%): Voting area and participants */}
       <div className="w-4/6 flex flex-col gap-6">
         {/* Voting controls and action buttons */}
         <div className="flex justify-between items-center">
@@ -20,7 +19,7 @@ const RoomWrapper = ({
       </div>
 
       {/* Right side (30%): Jira task details */}
-      <div className="w-3/6 bg-sky-50 p-6 border-l border-sky-200 overflow-auto">
+      <div className="w-3/6 bg-sky-50 border-l border-sky-200 overflow-auto">
         {children}
       </div>
     </div>
