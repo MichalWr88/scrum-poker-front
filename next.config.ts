118e86edc9b2f9696ac2d621bfc4b2afe6fd6d7e
mmaleszewski <maleszewski.geo@gmail.com>
2025-03-16 20:33:50 +0100

    feat: configure image domains and add environment variables for authentication

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  experimental: {
+    serverActions: {},
+  },
   env: {
     NEXT_PUBLIC_SOCKET_URL:
       process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3002", // Update with your actual socket server URL
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "",
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "",
   },
 };
 
