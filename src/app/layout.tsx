345dbf7a07773d92ee68e4029fa524c652aff2e9
mmaleszewski <maleszewski.geo@gmail.com>
2025-03-17 07:03:06 +0100

    feat: remove Navbar and Footer components from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Navbar } from "@/components/shared/navbar";
-import { Footer } from "@/components/shared/footer";
-
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
-        <Navbar />
         <main className="flex-1">{children}</main>
-        <Footer />
       </body>
     </html>
   );
