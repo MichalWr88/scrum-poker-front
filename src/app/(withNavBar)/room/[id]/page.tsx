f20355a1125898e1260bb09530ff803e0437a9dc
mmaleszewski <maleszewski.geo@gmail.com>
2025-03-23 08:48:33 +0100

    feat: add MongoDB connection initialization in RoomPage component

--- src/app/(withNavBar)/room/[id]/page.tsx
+++ src/app/(withNavBar)/room/[id]/page.tsx
@@ -3,8 +3,17 @@ import RoomWrapper from "@/src/components/room/room-wrapper";
 import ActionButtons from "@/src/components/room/action-buttons";
 import VotingArea from "@/src/components/room/voting-area";
 import { SessionProvider } from "next-auth/react";
+import { initConnectMongo } from "@/src/services/mongodb/service";
 
 export default async function RoomPage() {
+  try {
+    await initConnectMongo();
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("❌ Error connecting to Mongo:", error);
+    }
+  }
+
   return (
     <SessionProvider>
       <RoomWrapper
