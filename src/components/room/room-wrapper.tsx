b667a1c061b5fafa029fde985d723120daa96ee1
mmaleszewski <maleszewski.geo@gmail.com>
2025-04-05 18:07:42 +0200

    feat: integrate RoomPathGuard for room validation and enhance room data fetching

--- src/components/room/room-wrapper.tsx
+++ src/components/room/room-wrapper.tsx
@@ -1,5 +1,7 @@
 import { auth } from "@/auth";
+import scrumBackendService from "@/src/services/scrum-backend/scrum-backend.service";
 import { redirect } from "next/navigation"; // Use "next/navigation" for Next.js 13/15
+import RoomPathGuard from "./room-path-guard";
 
 const RoomWrapper = async ({
   children,
@@ -14,21 +16,24 @@ const RoomWrapper = async ({
   if (session === null) {
     redirect("/");
   }
-
+  const rooms = await scrumBackendService.getRooms();
+  console.log("Rooms:", rooms);
   return (
     <div className="flex gap-4 p-4 wrapper-footer-navbar">
-      <div className="flex flex-col gap-6 flex-grow ">
-        {/* Voting controls and action buttons */}
-        <div className="flex justify-between items-center">
-          {actionComponent}
-        </div>
+      <RoomPathGuard rooms={rooms}>
+        <>
+          <div className="flex flex-col gap-6 flex-grow ">
+            {/* Voting controls and action buttons */}
+            <div className="flex justify-between items-center">
+              {actionComponent}
+            </div>
 
-        {votingComponent}
-      </div>
+            {votingComponent}
+          </div>
 
-      <div className="bg-sky-50 border-l h-full">
-        {children}
-      </div>
+          <div className="bg-sky-50 border-l h-full">{children}</div>
+        </>
+      </RoomPathGuard>
     </div>
   );
 };
