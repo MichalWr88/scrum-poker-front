9b7e08662b854a09bd3f383662a66199d69e75a5
mmaleszewski <maleszewski.geo@gmail.com>
2025-04-08 08:17:18 +0200

    feat: update RoomWrapper to handle room fetching errors and improve layout

--- src/components/room/room-wrapper.tsx
+++ src/components/room/room-wrapper.tsx
@@ -1,5 +1,5 @@
 import { auth } from "@/auth";
-import scrumBackendService from "@/src/services/scrum-backend/scrum-backend.service";
+import scrumBackendService from "@/src/services/scrum-backend/scrum-backend.provider";
 import { redirect } from "next/navigation"; // Use "next/navigation" for Next.js 13/15
 import RoomPathGuard from "./room-path-guard";
 
@@ -16,26 +16,35 @@ const RoomWrapper = async ({
   if (session === null) {
     redirect("/");
   }
-  const rooms = await scrumBackendService.getRooms();
-  console.log("Rooms:", rooms);
-  return (
-    <div className="flex gap-4 p-4 wrapper-footer-navbar">
-      <RoomPathGuard rooms={rooms}>
-        <>
-          <div className="flex flex-col gap-6 flex-grow ">
-            {/* Voting controls and action buttons */}
-            <div className="flex justify-between items-center">
-              {actionComponent}
+  try {
+    const rooms = await scrumBackendService.getRooms();
+    console.log("Rooms:", rooms);
+    return (
+      <div className="flex gap-4 p-4 justify-center items-center wrapper-footer-navbar">
+        <RoomPathGuard rooms={rooms}>
+          <>
+            <div className="flex flex-col gap-6 flex-grow ">
+              {/* Voting controls and action buttons */}
+              <div className="flex justify-between items-center">
+                {actionComponent}
+              </div>
+
+              {votingComponent}
             </div>
 
-            {votingComponent}
-          </div>
-
-          <div className="bg-sky-50 border-l h-full">{children}</div>
-        </>
-      </RoomPathGuard>
-    </div>
-  );
+            <div className="bg-sky-50 border-l h-full">{children}</div>
+          </>
+        </RoomPathGuard>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching rooms:", error);
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold">Error fetching rooms</h1>
+      </div>
+    );
+  }
 };
 
 export default RoomWrapper;
