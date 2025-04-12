b95c5f69353a2e2fbac41a2adb67b5d8a7c867c5
mmaleszewski <maleszewski.geo@gmail.com>
2025-04-12 09:22:38 +0200

    feat: validate roomId in updateTaskKey and update room data accordingly

--- src/components/jira/jira-server-actions.ts
+++ src/components/jira/jira-server-actions.ts
@@ -2,16 +2,23 @@
 
 import { myServerProvider } from "@/src/services/my-server/my-server-provider";
 import { Task } from "./models/jira-task";
+import scrumBackendService from "@/src/services/scrum-backend/scrum-backend.provider";
 
 export async function updateTaskKey(
   formData: FormData
 ): Promise<Task | { error: string }> {
   try {
     const newKey = formData.get("key") as string;
+    const roomId = formData.get("roomId") as string;
+    if (!roomId) {
+      throw new Error("No roomId provided");
+    }
     if (!newKey) {
       throw new Error("No key provided");
     }
     const updatedTask = await myServerProvider.getTaskById(newKey);
+    await scrumBackendService.editRoom(roomId, { lastSubject: newKey });
+    await scrumBackendService.addSubject(newKey, { jiraId: newKey });
     return updatedTask;
   } catch (error: unknown) {
     console.error("Error in updateTaskKey:", error);
