4a495dac4c57842d8dbd8e93ffdeb70531c97030
mmaleszewski <maleszewski.geo@gmail.com>
2025-04-12 09:01:01 +0200

    feat: update RoomPage to accept props and fetch room data for JiraTask

--- src/app/(withNavBar)/room/[id]/page.tsx
+++ src/app/(withNavBar)/room/[id]/page.tsx
@@ -4,8 +4,10 @@ import ActionButtons from "@/src/components/room/action-buttons";
 import VotingArea from "@/src/components/room/voting-area";
 import { SessionProvider } from "next-auth/react";
 import { initConnectMongo } from "@/src/services/mongodb/service";
+import scrumBackendService from "@/src/services/scrum-backend/scrum-backend.provider";
+import { PageProps } from "@/.next/types/app/page";
 
-export default async function RoomPage() {
+export default async function RoomPage(props: PageProps) {
   try {
     await initConnectMongo();
   } catch (error: unknown) {
@@ -13,14 +15,15 @@ export default async function RoomPage() {
       console.error("❌ Error connecting to Mongo:", error);
     }
   }
-
+  const params = await props?.params;
+  const roomData = await scrumBackendService.getRoom(params.id);
   return (
     <SessionProvider>
       <RoomWrapper
         actionComponent={<ActionButtons />}
         votingComponent={<VotingArea />}
       >
-        <JiraTask />
+        <JiraTask taskId={roomData.lastSubject} />
       </RoomWrapper>
     </SessionProvider>
   );
